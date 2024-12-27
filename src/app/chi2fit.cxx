2f2169db1ca2d66b7bd53e6472a5fcbcedaf0da7
Qiyu Yan <yanqiyu01@gmail.com>
2024-12-27 18:57:59 +0800

    chi2 contour

--- src/app/chi2fit.cxx
+++ src/app/chi2fit.cxx
@@ -1,12 +1,20 @@
 #include "SimpleDataHist.h"
 #include "binning_tool.hpp"
 #include <BinnedInteraction.h>
+
 #include <Minuit2/FCNBase.h>
 #include <Minuit2/FunctionMinimum.h>
+#include <Minuit2/MnContours.h>
 #include <Minuit2/MnMigrad.h>
 #include <Minuit2/MnUserParameters.h>
+
+#include <TCanvas.h>
+#include <TGraph.h>
+
+#include <algorithm>
 #include <cmath>
 #include <print>
+#include <ranges>
 
 class MinuitFitter final : public ROOT::Minuit2::FCNBase {
 public:
@@ -23,8 +31,6 @@ public:
     binned_interaction.UpdatePrediction();
     auto llh = binned_interaction.GetLogLikelihoodAgainstData(data);
     llh += binned_interaction.GetLogLikelihood();
-    std::println("{}", llh);
-    // return -(binned_interaction.GetLogLikelihoodAgainstData(data));
     return -2. * llh;
   }
 
@@ -48,16 +54,17 @@ int main(int argc, char **agrv) {
   BinnedInteraction bint{Ebins, costheta_bins, scale_factor, 40, 40, 1};
   auto cdata = bint.GenerateData(); // data for NH
 
-  std::println("event count for NH{}", cdata.hist_numu.GetSum());
+  std::println("event count for NH: {:.2f}", cdata.hist_numu.GetSum());
 
   MinuitFitter fitter_IH(bint, cdata);
   ROOT::Minuit2::MnUserParameters param{};
-  param.Add("DM2", -2.529e-3, 0.029e-3);
-  param.Add("Dm2", 7.53e-5, 0.18e-5);
-  param.Add("T23", 0.553, 0.022);
-  param.Add("T13", 2.19e-2, 0.07e-2);
-  param.Add("T12", 0.307, 0.013);
-  param.Add("DCP", 1.36, 0.2);
+  param.Add("#Delta M_{32}^{2}", -2.529e-3, 0.029e-3, -1, 0);
+  // param.Add("#Delta M_{32}^{2}", 2.529e-3, 0.029e-3, 0, 1);
+  param.Add("#Delta M_{21}^{2}", 7.53e-5, 0.18e-5, 0, 1);
+  param.Add("sin^{2}#theta_{23}", 0.553, 0.022, 0, 1);
+  param.Add("sin^{2}#theta_{13}", 2.19e-2, 0.07e-2, 0, 1);
+  param.Add("sin^{2}#theta_{12}", 0.307, 0.013, 0, 1);
+  param.Add("#delta_{CP}", 1.36 * M_PI, 0.2);
 
   auto result = ROOT::Minuit2::MnMigrad{fitter_IH, param}();
 
@@ -67,6 +74,43 @@ int main(int argc, char **agrv) {
   }
 
   auto minimal_chi2 = result.Fval();
+
   std::println("Fval: {}", minimal_chi2);
+
+  ROOT::Minuit2::MnContours contours(fitter_IH, result);
+  for (size_t i = 0; i < final_params.Params().size(); ++i) {
+    for (size_t j = i + 1; j < final_params.Params().size(); ++j) {
+      const size_t npoints = 15;
+      auto contour = contours(i, j, npoints);
+      std::println("Contour between {} and {}:", final_params.GetName(i),
+                   final_params.GetName(j));
+      std::array<double, npoints + 1> x{}, y{};
+      std::ranges::for_each(std::views::zip(contour, x, y),
+                            [](auto &&point_x_y) {
+                              auto &[point, x, y] = point_x_y;
+                              x = point.first;
+                              y = point.second;
+                            });
+      x[npoints] = x[0];
+      y[npoints] = y[0];
+      TCanvas canvas;
+      auto left_margin = canvas.GetLeftMargin();
+      canvas.SetLeftMargin(left_margin * 1.5);
+
+      auto right_margin = canvas.GetRightMargin();
+      canvas.SetRightMargin(right_margin * 0.5);
+
+      TGraph graph(npoints + 1, x.data(), y.data());
+      graph.SetTitle("");
+      graph.GetXaxis()->SetTitle(final_params.GetName(i).c_str());
+      graph.GetYaxis()->SetTitle(final_params.GetName(j).c_str());
+
+      graph.Draw("AC");
+      canvas.SaveAs(std::format("contour_{}_{}.pdf", final_params.GetName(i),
+                                final_params.GetName(j))
+                        .c_str());
+    }
+  }
+
   return 0;
 }
